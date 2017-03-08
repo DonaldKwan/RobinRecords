d1c652345f2948ea7641297a0ed0463db201a31d
Erik Ahlbrecht <ahlberik@gmail.com>
2017-03-08 00:57:24 -0600

    Fixed rounding issues, I think this should work correctly for money

--- market/Position.java
+++ market/Position.java
@@ -1,30 +1,32 @@
 package com.eahlbrecht.robinrecords.market;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by defq0n on 3/4/17.
  *
  * Keep track of current stock position
- *
- * TODO share price and totalprice to BigDecimal
  */
 public class Position {
 
     private final String TICKER;
-    private final double SHARE_PRICE;
+    private final BigDecimal SHARE_PRICE;
     private final int SHARE_AMOUNT;
     private final Date DATE;
     private final ORDER ORDER_TYPE;
-    private double totalPrice;
+    private final BigDecimal totalPrice;
 
     public Position(String ticker, double sharePrice, int shareAmount, Date date, ORDER order){
         TICKER = ticker;
-        SHARE_PRICE = sharePrice;
+        SHARE_PRICE = BigDecimal.valueOf(sharePrice);
         SHARE_AMOUNT = shareAmount;
         DATE = date;
         ORDER_TYPE = order;
-        totalPrice = sharePrice * shareAmount;
+        totalPrice = new BigDecimal(sharePrice * shareAmount).setScale(2, RoundingMode.HALF_EVEN);
     }
 
 
@@ -32,7 +34,7 @@ public class Position {
         return TICKER;
     }
 
-    public double getSharePrice() {
+    public BigDecimal getSharePrice() {
         return SHARE_PRICE;
     }
 
@@ -48,7 +50,7 @@ public class Position {
         return ORDER_TYPE;
     }
 
-    public double getTotalPrice(){
+    public BigDecimal getTotalPrice(){
         return totalPrice;
     }
 
@@ -58,7 +60,7 @@ public class Position {
                 .append(SHARE_PRICE).append(",")
                 .append(SHARE_AMOUNT).append(",")
                 .append(DATE.toString()).append(",")
-                .append(totalPrice).append(",")
+                .append(totalPrice.doubleValue()).append(",")
                 .append(ORDER_TYPE.getString()).append(">").toString();
     }
 
